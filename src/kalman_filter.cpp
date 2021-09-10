7adc644591944aa8d0b5002e41710aa47ff71123
Karthiek Naagaraj <knaaga90@gmail.com>
2021-09-10 01:25:37 -0400

    Implemented predict and update methods for laser and radar

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,59 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose()+ Q_;
 }
 
-void KalmanFilter::Update(const VectorXd &z) {
+void KalmanFilter::Update(const VectorXd &meas) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  std::cout <<"Laser update"<<std::endl;
+  VectorXd z = VectorXd(2);
+  z << meas[0], meas[1];
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
+void KalmanFilter::UpdateEKF(const VectorXd &meas) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  std::cout <<"Radar update"<<std::endl;
+  VectorXd z = VectorXd(3);
+  z << meas[0], meas[1], meas[2];
+
+  VectorXd z_pred = VectorXd(3);
+  double px = x_[0];
+  double py = x_[1];
+  double vx = x_[2];
+  double vy = x_[3];
+
+  double rho = sqrt(px*px + py*py);
+  double phi = atan(py/px);
+  double rho_dot = (px*vx + py*vy)/sqrt(px*px + py*py);
+
+  while (phi <=-M_PI) { phi += (M_PI*2); }
+  while (phi >= M_PI) { phi -= (M_PI*2); }
+
+  z_pred << rho, phi, rho_dot;
+
+  VectorXd y = z - z_pred;
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
